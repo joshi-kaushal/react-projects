f1a0dca6882a49310904298418e81d9b85b0c0e9
Kaushal Joshi <7joshikaushal@gmail.com>
2021-11-29 22:57:01 +0530

    08 Lorem Tailwind Prettified

--- src/Projects/LoremIpsum/LoremIpsum.js
+++ src/Projects/LoremIpsum/LoremIpsum.js
@@ -22,20 +22,20 @@ const LoremIpsum = () => {
 	}
 
 	return(
-		<section className='w-90vw md:w-95vw my-0 mx-auto max-w-2xl mt-20 text-center'>
-			<h3 className='text-loremColor-primary text-2xl font-semibold'>Lorem Ipsum Generator</h3>
-			<form className='capitalize leading-8 mt-8 mb-16 flex justify-center items-center' onSubmit={handleSubmit}>
+		<section className='max-w-2xl mx-auto my-0 mt-20 text-center w-90vw md:w-95vw'>
+			<h3 className='text-2xl font-semibold text-loremColor-primary'>Lorem Ipsum Generator</h3>
+			<form className='flex items-center justify-center mt-8 mb-16 leading-8 capitalize' onSubmit={handleSubmit}>
 				<label htmlFor='amount' className='text-xl text-loremColor-primary'>Paragraphs:</label>
 				<input 
 					id='amount'
-					className='py-1 px-2 w-16 border-none rounded-md my-0 mx-2 text-xl'
+					className='w-16 px-2 py-1 mx-2 my-0 text-xl border-none rounded-md'
 					type='number' 
 					name='amount' 
 					value={count} 
 					onChange={(e) => setCount(e.target.value)}
 				/>
 
-				<button className='uppercase bg-loremColor-secondary hover:bg-loremColor-ternary text-loremColor-primary hover:text-loremColor-secondary py-1 px-3 h-8 leading-3 inline-block transition duration-300 ease-in text-sm border-2 border-loremColor-secondary hover:border-loremColor-ternary cursor-pointer shadow-lightShadow hover:shadow-darkShadow rounded-md'>Generate</button>
+				<button className='inline-block h-8 px-3 py-1 text-sm leading-3 uppercase transition duration-300 ease-in border-2 rounded-md cursor-pointer bg-loremColor-secondary hover:bg-loremColor-ternary text-loremColor-primary hover:text-loremColor-secondary border-loremColor-secondary hover:border-loremColor-ternary shadow-lightShadow hover:shadow-darkShadow'>Generate</button>
 			</form>
 
 			<article className='lorem-text'>
