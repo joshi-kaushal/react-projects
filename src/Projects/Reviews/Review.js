599f85b47707aeae99ad526f2d1e405a6839f5e5
Kaushal Joshi <7joshikaushal@gmail.com>
2021-12-01 23:46:16 +0530

    07 Reviews Slider Tailwind prettified

--- src/Projects/Reviews/Review.js
+++ src/Projects/Reviews/Review.js
@@ -37,30 +37,30 @@ const Review = () => {
 	}
 
 
-	return <article className="bg-white py-6 px-8 border rounded-md shadow-lightShadow transition duration-300 ease-linear text-center hover:shadow-darkShadow">
+	return <article className="px-8 py-6 text-center transition duration-300 ease-linear bg-white border rounded-md shadow-lightShadow hover:shadow-darkShadow">
 		<div className='img-container'>
 			<img src={image} alt={name} className='person-img'></img>
-			<span className='absolute top-0 left-0 w-10 h-10 grid place-items-center border rounded-50 transform translate-y-1/4 bg-reviewsColor-primaryLight text-white'>
+			<span className='absolute top-0 left-0 grid w-10 h-10 text-white transform border place-items-center rounded-50 translate-y-1/4 bg-reviewsColor-primaryLight'>
 				<FaQuoteRight />
 			</span>
 		</div>
 
-		<h4 className='mb-1 uppercase font-bold text-xl'>{name}</h4>
+		<h4 className='mb-1 text-xl font-bold uppercase'>{name}</h4>
 		<p className='mb-2 uppercase text-reviewsColor-primaryLight text-md'>{job}</p>
 		<p className='mb-3'>
 			{text}
 		</p>
 
 		<div className='button-container'>
-			<button className='text-reviewsColor-secondaryDark text-xl bg-transparent border-transparent my-0 mx-2 transition duration-300 ease-out cursor-pointer hover:text-reviewsColor-primaryLight' onClick={prevPerson}>
+			<button className='mx-2 my-0 text-xl transition duration-300 ease-out bg-transparent border-transparent cursor-pointer text-reviewsColor-secondaryDark hover:text-reviewsColor-primaryLight' onClick={prevPerson}>
 				<FaChevronLeft />
 			</button>
-			<button className='text-reviewsColor-secondaryDark text-xl bg-transparent border-transparent my-0 mx-2 transition duration-300 ease-out cursor-pointer hover:text-reviewsColor-primaryLight' onClick={nextPerson}>
+			<button className='mx-2 my-0 text-xl transition duration-300 ease-out bg-transparent border-transparent cursor-pointer text-reviewsColor-secondaryDark hover:text-reviewsColor-primaryLight' onClick={nextPerson}>
 				<FaChevronRight />
 			</button>
 		</div>
 
-		<button className='mt-2 bg-reviewsColor-secondarySecondary text-reviewsColor-primaryLight py-1 px-2 capitalize border rounded-2xl transition duration-300 ease-in cursor-pointer hover:bg-reviewsColor-primaryLight hover:text-reviewsColor-primaryDark' onClick={randomPerson}>
+		<button className='px-2 py-1 mt-2 capitalize transition duration-300 ease-in border cursor-pointer bg-reviewsColor-secondarySecondary text-reviewsColor-primaryLight rounded-2xl hover:bg-reviewsColor-primaryLight hover:text-reviewsColor-primaryDark' onClick={randomPerson}>
 			Random
 		</button>
 
--- src/Projects/Reviews/Reviews.js
+++ src/Projects/Reviews/Reviews.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Review from './Review';
 
 function Reviews() {
-	return <main className='min-h-screen grid place-items-center bg-reviewsColor-secondarySecondary'>
+	return <main className='grid min-h-screen place-items-center bg-reviewsColor-secondarySecondary'>
 		<section className='w-880'>
-			<div className='text-center mb-16'>
+			<div className='mb-16 text-center'>
 				<h2 className='text-3xl font-bold'>Reviews</h2>
 				<div className='underline'></div>
 			</div>
