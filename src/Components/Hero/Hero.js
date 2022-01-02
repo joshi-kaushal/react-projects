960e2fda7d709f70057009c8b85a0ca6c918e672
Kaushal Joshi <7joshikaushal@gmail.com>
2022-01-02 22:25:34 +0530

    grid layout added

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -5,31 +5,34 @@ import { GoLinkExternal } from 'react-icons/go'
 
 import projects from '../../Projects';
 
+const RenderCards = (props) => {
+	return projects.map(project => {
+		return (
+			<section className='flex items-center justify-between h-40 px-4 py-2 mx-auto my-0 mb-4 transition-all duration-300 ease-in-out border border-none rounded-md w-85vw md:w-full bg-palette-card hover:bg-palette-hover shadow-darkShadow'>
+				<div className='mr-3 '>
+					<h3 className='font-serif text-2xl font-semibold md:text-2xl'>
+						{project.name}
+					</h3>
+					<p className='text-lg font-semibold md:text-xl'>
+						{project.description}
+					</p>
+				</div>
+
+				<Link to={`${project.slug}`} className='flex items-center w-16 p-2 text-white transition-all duration-300 ease-in-out border rounded-md hover:border-palette-buttons bg-palette-buttons border-palette-buttons justify-items-end hover:bg-palette-card hover:text-black '>
+					Visit <span className='ml-1'><GoLinkExternal /></span>
+				</Link>
+			</section>
+		)
+	})
+}
 
 const Hero = (props) => {
-	return <main className='bg-palette-background'>	
+	return <main className='bg-palette-background'>
 		<h2 className='font-serif text-center '>Project List</h2>
 
-		{
-			projects.map(project => {
-				return (
-					<section className='flex items-center justify-between p-4 mx-auto my-0 mb-4 transition-all duration-300 ease-in-out border border-none rounded-md bg-palette-card hover:bg-palette-hover shadow-darkShadow w-85vw md:w-3/5'>
-						<div className='mr-3 '>
-							<h3 className='font-serif text-2xl font-semibold md:text-2xl'>
-								{project.name}
-							</h3>
-							<p className='text-lg font-semibold md:text-xl'>
-								{project.description}
-							</p>
-						</div>
-
-						<Link to={`${project.slug}`} className='flex items-center w-16 p-2 text-white transition-all duration-300 ease-in-out rounded-md bg-palette-buttons border-palette-buttons justify-items-end hover:bg-palette-card hover:text-black '>
-							Visit <span className='ml-1'><GoLinkExternal /></span>
-						</Link>
-					</section>
-				)
-			})
-		}
+		<div className="grid mx-auto my-0 mt-20 lg:grid-cols-2 xl:grid-cols-3 md:gap-10 w-85vw place-content-center">
+			<RenderCards />
+		</div>
 	</main>
 }
 
